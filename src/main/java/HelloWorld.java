c2f829d48b1cb8475d835044caca0c029e550a5c
Cezary Ciślak <91832579+s25429@users.noreply.github.com>
2024-06-21 12:23:40 +0200

    Add files via upload

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,9 +1,9 @@
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println(getMessage());
+    public static String getMessage() {
+        return "Hello, World!";
     }
 
-    public String getMessage() {
-        return "Hello, World!";
+    public static void main(String[] args) {
+        System.out.println(getMessage());
     }
 }
